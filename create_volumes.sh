67dd11f8195b319cf0b9c1194ab0cfb561106885
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-05 21:00:10 +0100

    passing LABEL

--- create_volumes.sh
+++ create_volumes.sh
@@ -6,13 +6,13 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 # shellcheck source=volumes.inc
 . "$DIR/volumes.inc"
 
-DEFAULT_LABEL=LIVE
 VOLUME_STACKNAME=gocd-vol
+DEFAULT_LABEL=LIVE
 
 _GO_PIPELINE_COUNTER=-${GO_PIPELINE_COUNTER:-0}
 
 VOLUME_STACKNAME=$VOLUME_STACKNAME$_GO_PIPELINE_COUNTER
 
 createVolumes \
-  "${1:-$DEFAULT_LABEL}" \
-  "${2:-$VOLUME_STACKNAME}"
+  "${1:-$VOLUME_STACKNAME}" \
+  "${2:-$DEFAULT_LABEL}"
