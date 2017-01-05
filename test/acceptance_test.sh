c9f7f382fbb613eea0709afae6b0c8565602e322
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-05 20:57:28 +0100

    initial check-in

---
+++ test/acceptance_test.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+# shellcheck source=../server.inc
+. "$DIR/../server.inc"
+# shellcheck source=../cluster.inc
+. "$DIR/../cluster.inc"
+# shellcheck source=../volumes.inc
+. "$DIR/../volumes.inc"
+
+SERVER_STACKNAME=test-svm
+CLUSTER_STACKNAME=test-svc
+VOLUMES_STACKNAME=test-vol
+
+_GO_PIPELINE_COUNTER=-${GO_PIPELINE_COUNTER:-0}
+
+SERVER_STACKNAME=$SERVER_STACKNAME$_GO_PIPELINE_COUNTER
+CLUSTER_STACKNAME=$CLUSTER_STACKNAME$_GO_PIPELINE_COUNTER
+VOLUMES_STACKNAME=$VOLUMES_STACKNAME$_GO_PIPELINE_COUNTER
+
+SHUNIT=$(which shunit)
+
+
+
+function testCreateVolumes() {
+  createVolumes "$VOLUMES_STACKNAME" "TEST"
+  assertEquals "createVolumes failed" 0 $?
+}
+
+
+
+function testDestroyVolumes() {
+  destroyVolumes "$VOLUMES_STACKNAME"
+  assertEquals "destroyVolumes failed" 0 $?
+}
+
+
+
+# function oneTimeSetup()
+# function oneTimeTearDown()
+# function setup()
+# function teardown()
+
+
+
+# shellcheck source=/usr/local/bin/shunit
+. "$SHUNIT"
