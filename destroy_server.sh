932dbb83e304adb0a992f1adfb0301561643112b
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-13 10:53:13 +0100

    exit, not return + experimental code

--- destroy_server.sh
+++ destroy_server.sh
@@ -6,16 +6,31 @@
 # shellcheck source=volumes.inc
 . "$(dirname "$BASH_SOURCE")/volumes.inc"
 
-ENV=${1:-init}
+STACK_SUFFIX=${1:-init-0}
 DESTROY_VOLUMES=${2:-false}
+
+shopt -s extglob
+
+ENV=
+if [ -z "${STACK_SUFFIX//+([0-9])}" ] ; then
+  GO_PIPELINE_COUNTER="$STACK_SUFFIX"
+else
+  ENV=${STACK_SUFFIX##+([0-9])-}
+  if [ -n "$ENV" ] ; then
+    GO_PIPELINE_COUNTER=${STACK_SUFFIX%%$ENV}
+    GO_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER%%-}
+  fi
+fi
+
+#echo "ENV \"$ENV\" GO_PIPELINE_COUNTER \"$GO_PIPELINE_COUNTER\""
 
 GOCD_SVC="$(getStackname "gocd-svc" "$ENV")"
 GOCD_SVM="$(getStackname "gocd-svm" "$ENV")"
 GOCD_VOL="$(getStackname "gocd-vol" "$ENV")"
 
-destroyCluster  "$GOCD_SVC" || return $?
-destroyMachine  "$GOCD_SVM" || return $?
+destroyCluster  "$GOCD_SVC" || exit $?
+destroyMachine  "$GOCD_SVM" || exit $?
 
-[ "$DESTROY_VOLUMES" = "destroy-volumes" ] || return
+[ "$DESTROY_VOLUMES" = "destroy-volumes" ] || exit 0
 
-destroyVolumes  "$GOCD_VOL" || return $?
+destroyVolumes  "$GOCD_VOL" || exit $?
