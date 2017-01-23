13d059a8e9c2bc68b0acc60a39b41d796be2ece8
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-23 20:33:54 +0100

    added update-stack functionality

--- create_server.sh
+++ create_server.sh
@@ -10,5 +10,7 @@ ENVLABEL=${1:-live}
 
 FROM=${2:-live}   # label of volume to copy, e.g. "none", "live"
 
-createVolumes "$ENVLABEL" "$FROM" || exit $?
-createServer  "$ENVLABEL"         || exit $?
+UPDATE=$3
+
+createVolumes "$ENVLABEL" "$FROM"   || exit $?
+createServer  "$ENVLABEL" "$UPDATE" || exit $?
