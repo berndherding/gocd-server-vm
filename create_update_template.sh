5b9a310fe96d8b149cd481e1a6131919bafc16b8
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-24 12:50:26 +0100

    fixed missing "

--- create_update_template.sh
+++ create_update_template.sh
@@ -4,4 +4,4 @@
   awk '/"Resources" *: *{/ {print; p=1; next} !p' create-server.cf 
   cat update.cf 
   awk '/"Resources" *: *{/ {       p=1; next}  p' create-server.cf
-) > "$(dirname "${BASH_SOURCE[0]}")/update-server.cf
+) > "$(dirname "${BASH_SOURCE[0]}")/update-server.cf"
