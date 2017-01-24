73c449ea996018c2efc6a46fbcc18dfa8ac18ab8
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-24 15:56:56 +0100

    DESTROY_VOLUMES not needed any longer

--- destroy_server.sh
+++ destroy_server.sh
@@ -7,12 +7,8 @@
 . "$(dirname "$BASH_SOURCE")/volumes.inc"
 
 ENVLABEL=${1:-live}
-DESTROY_VOLUMES=${2:-do-not-destroy-volumes}
 
 shopt -s extglob
 
 destroyServer  "$ENVLABEL" || exit $?
-
-[ "$DESTROY_VOLUMES" = "destroy-volumes" ] || exit 0
-
 destroyVolumes "$ENVLABEL" || exit $?
