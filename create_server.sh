c75ad175f2740434f41f2834f762f2b808842159
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-25 13:26:32 +0100

    dropped createVolumes

--- create_server.sh
+++ create_server.sh
@@ -6,9 +6,7 @@
 # shellcheck source=volumes.inc
 . "$(dirname "$BASH_SOURCE")/volumes.inc"
 
-ENVLABEL=${1:-live}
-
+ENV=${1:-live}
 FROM=${2:-live}   # label of volume to copy, e.g. "none", "live"
 
-createVolumes "$ENVLABEL" "$FROM" || exit $?
-createServer  "$ENVLABEL"         || exit $?
+createServer "$ENV" "$FROM" || exit $?
