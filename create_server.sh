d9c90ec11cf0a189d005491181d7f7749e1e0476
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-26 12:04:12 +0100

    deleted ref to volumes.inc

--- create_server.sh
+++ create_server.sh
@@ -3,9 +3,6 @@
 # shellcheck source=server.inc
 . "$(dirname "$BASH_SOURCE")/server.inc"
 
-# shellcheck source=volumes.inc
-. "$(dirname "$BASH_SOURCE")/volumes.inc"
-
 ENV=${1:-live}
 FROM=${2:-live}   # label of volume to copy, e.g. "none", "live"
 
--- destroy_server.sh
+++ destroy_server.sh
@@ -3,12 +3,6 @@
 # shellcheck source=server.inc
 . "$(dirname "$BASH_SOURCE")/server.inc"
 
-# shellcheck source=volumes.inc
-. "$(dirname "$BASH_SOURCE")/volumes.inc"
+ENV=${1:-live}
 
-ENVLABEL=${1:-live}
-
-shopt -s extglob
-
-destroyServer  "$ENVLABEL" || exit $?
-destroyVolumes "$ENVLABEL" || exit $?
+destroyServer "$ENV" || exit $?
