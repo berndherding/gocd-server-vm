2f1adce3f76a4d94b086a131dbc7fbcdd7fc50d1
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-26 12:03:20 +0100

    deleted

--- create_volumes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck source=volumes.inc
-. "$(dirname "$BASH_SOURCE")/volumes.inc"
-
-ENVLABEL=${1:-test}
-FROM=${2:-none}       # "none", "live"
-
-createVolumes "$ENVLABEL" "$FROM" || exit $?
--- destroy_volumes.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck source=volumes.inc
-. "$(dirname "$BASH_SOURCE")/volumes.inc"
-
-ENVLABEL=${1:-test}
-
-destroyVolumes "$ENVLABEL" || exit $?
