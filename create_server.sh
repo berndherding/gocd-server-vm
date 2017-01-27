3a8279cc49104e19391d2711565a3fb0211d9bfa
Bernd Herding <berndherding@users.noreply.github.com>
2017-01-27 12:39:36 +0100

    added $INSTANCE_TYPE

--- create_server.sh
+++ create_server.sh
@@ -5,5 +5,6 @@
 
 ENV=${1:-live}
 FROM=${2:-live}   # label of volume to copy, e.g. "none", "live"
+INSTANCE_TYPE=${3:-t2.medium}
 
-createServer "$ENV" "$FROM" || exit $?
+createServer "$ENV" "$FROM" "$INSTANCE_TYPE" || exit $?
