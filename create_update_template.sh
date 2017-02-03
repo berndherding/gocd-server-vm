ea34dc0412b47f6f61bfb72ae851dda9ba94bccb
Bernd Herding <berndherding@users.noreply.github.com>
2017-02-03 13:33:06 +0100

    renamed

--- create_update_template.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-(
-  awk '/"Resources" *: *{/ {print; p=1; next} !p' create-server.cf 
-  cat update.cf 
-  awk '/"Resources" *: *{/ {       p=1; next}  p' create-server.cf
-) > "$(dirname "${BASH_SOURCE[0]}")/update-server.cf"
